949a5c526f28e4e3edceeddbad539c6f057c3989
heejinjinjin <sshj2917@gmail.com>
2024-09-24 14:39:37 +0900

    BoardService에 getList() BoardServiceImpl getList()구현

--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -1,12 +1,17 @@
 package com.example.board.service;
 
 import com.example.board.dto.BoardDTO;
+import com.example.board.dto.PageRequestDTO;
+import com.example.board.dto.PageResultDTO;
 import com.example.board.entity.Borad;
 import com.example.board.entity.Member;
 
 public interface BoardService {
     // 새 글을 등록하는 기능
     Long register(BoardDTO dto);
+
+    // 게시목록 처리 기능
+    PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO);
 
     // Entity를 DTO로 변환하는 메소드
     default BoardDTO entityToDTO(Borad board, Member member, Long replyCount){
--- src/main/java/com/example/board/service/BoardServiceImpl.java
+++ src/main/java/com/example/board/service/BoardServiceImpl.java
@@ -1,10 +1,17 @@
 package com.example.board.service;
 
 import com.example.board.dto.BoardDTO;
+import com.example.board.dto.PageRequestDTO;
+import com.example.board.dto.PageResultDTO;
 import com.example.board.entity.Borad;
+import com.example.board.entity.Member;
 import com.example.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +23,14 @@ public class BoardServiceImpl implements BoardService {
         repository.save(board);
 
         return board.getBno();
+    }
+
+    @Override
+    public PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO) {
+
+        Function<Object[], BoardDTO> fn = (en -> entityToDTO((Borad) en[0], (Member) en[1], (Long) en[2]));
+        Page<Object[]> result = repository.getBoardWithReplyCount(pageRequestDTO.getPageable(Sort.by("bno").descending()));
+
+        return new PageResultDTO<>(result, fn);
     }
 }
